9ac6c0882114f6de2dd9f607996ab469bd02eae0
André Martins <andrebertonhadev@gmail.com>
2019-11-20 00:15:31 -0300

    update: Post Model relation with user

--- app/Models/Post.js
+++ app/Models/Post.js
@@ -1,9 +1,11 @@
 'use strict'
 
-/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Model = use('Model')
 
 class Post extends Model {
+  user () {
+    return this.belongsTo('App/Model/User')
+  }
 }
 
 module.exports = Post
