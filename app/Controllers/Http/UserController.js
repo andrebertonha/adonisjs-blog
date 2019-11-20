a7431ae0aef87684ba514cfcc67899519b80a6bb
André Martins <andrebertonhadev@gmail.com>
2019-11-20 13:27:24 -0300

    acl with middleware

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -3,6 +3,12 @@
 const User = use('App/Models/User')
 
 class UserController {
+  async index () {
+    const users = await User.all()
+
+    return users
+  }
+
   async store ({ request }) {
     const { permissions, roles, ...data } = request.only([
       'username',
--- start/routes.js
+++ start/routes.js
@@ -2,14 +2,22 @@
 
 const Route = use('Route')
 
+Route.get('users', 'UserController.index')
 Route.post('users', 'UserController.store')
 Route.put('/users/:id', 'UserController.update')
 
 Route.post('sessions', 'SessionController.store')
 
-Route.group(() => {
-  Route.resource('posts', 'PostController').apiOnly()
-}).middleware(['auth'])
+Route.resource('/posts', 'PostController')
+  .apiOnly()
+  .except(['index', 'show'])
+  .middleware(['auth', 'is:(administrator || moderator)'])
+
+Route.get('/posts', 'PostController.index')
+  .middleware(['auth', 'can:read_posts'])
+
+Route.get('/posts/:id', 'PostController.show')
+  .middleware(['auth', 'can:read_posts'])
 
 Route.resource('permissions', 'PermissionController').apiOnly().middleware('auth')
 
